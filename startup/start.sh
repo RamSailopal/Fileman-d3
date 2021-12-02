4029214e463832dd355dd683c3c45db7d09825a8
RamSailopal <thelinuxguys@yahoo.com>
2021-12-02 12:02:23 +0000

    Hasdn't actually started Apache

--- startup/start.sh
+++ startup/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp -Rf /home/htdocs/* /opt/bitnami/apache/htdocs/ && find /opt/bitnami/apache/htdocs/ -type f -name "*.html" | xargs sed -i 's@\$NODE_FMQLADD@'$NODE_FMQLADD'@'
 /opt/bitnami/scripts/apache/entrypoint.sh
-tail -f /dev/null
+/opt/bitnami/scripts/apache/run.sh
